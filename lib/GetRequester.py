913c058122db1c8c7f4a5b3d16fd20a3210a24cb
ochiengcynthia <126596182+ochiengcynthia@users.noreply.github.com>
2023-06-22 15:16:04 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,11 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.text
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+        return json.loads(response_body)
         pass
